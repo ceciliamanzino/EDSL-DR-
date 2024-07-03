cb6d5b3eee9720d76f8c6f3141ccf1282a570a5b
ceciliamanzino <70380080+ceciliamanzino@users.noreply.github.com>
2024-07-03 15:41:18 -0300

    Update Interpreter.hs

--- DR/Interpreter.hs
+++ DR/Interpreter.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module DR.Interpreter where
+module Interpreter where
 
-import DR.AbstractSyntax
+import AbstractSyntax
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import Control.Monad.State 
@@ -15,17 +15,12 @@ type Env = M.Map Int Int
 initMemory :: Env
 initMemory = M.empty
 
-
 type Result a = StateT Env Maybe a
   
 
--- convert a value of type SNat n to a natural of type Int that it represents    
-toInt :: SNat n -> Int
-toInt SZero = 0
-toInt (SSucc x) = 1 + (toInt x)
-
 update :: Int -> Int -> Result ()   
 update v i = StateT (\s -> Just ((), M.insert v i s))  
+
 
 lookfor :: Int -> Result Int 
 lookfor x = do env <- get
@@ -50,64 +45,26 @@ evalIni p =  eval p initMemory
 ---------------------------------------
 
 evalExp ::  Exp a b c d -> Result Int
-evalExp (IntLit i)        = return i
-evalExp (BoolLit i)       = return (boolToInt i)
-evalExp (Var x)           = lookfor (toInt x)
-evalExp (Declassify e l)  = evalExp e 
-evalExp (Ope Plus e1 e2)  = evalIntOp e1 e2 (+)
-evalExp (Ope Minus e1 e2) = evalIntOp e1 e2 (-)
-evalExp (Ope Mult e1 e2)  = evalIntOp e1 e2 (*)
-evalExp (Ope Div e1 e2)   = do v1 <- evalExp e1
-                               v2 <- evalExp e2
-                               return (div v1 v2)
-evalExp (Ope Exp e1 e2)   = evalIntOp e1 e2 (^)
-evalExp (Ope Mod e1 e2)   = evalIntOp e1 e2 (mod)
-evalExp (Ope And e1 e2)   = evalIntOp e1 e2 (andOp) 
-evalExp (Ope Or e1 e2)    = evalIntOp e1 e2 (orOp) 
-evalExp (Ope Gt e1 e2)    = evalBoolOp e1 e2 (>)
-evalExp (Ope GtE e1 e2)   = evalBoolOp e1 e2 (>=) 
-evalExp (Ope Lt e1 e2)    = evalBoolOp e1 e2 (<) 
-evalExp (Ope LtE e1 e2)   = evalBoolOp e1 e2 (<=) 
-evalExp (Ope Eq e1 e2)    = evalBoolOp e1 e2 (==) 
-evalExp (Ope NotEq e1 e2) = evalBoolOp e1 e2 (/=) 
+evalExp (IntLit i)                = return i
+evalExp (BoolLit i)               = return (fromBool i)
+evalExp (Var x)                   = lookfor (toInt x)
+evalExp (Declassify e l)          = evalExp e 
+evalExp (OpBin (BinInt f) e1 e2)  = evalOp e1 e2 f
+evalExp (OpBin (PredInt f) e1 e2) = evalOp e1 e2 (\v v' -> fromBool (f v v'))
+evalExp (OpBin (BinBool f) e1 e2) = evalOp e1 e2 (\v v' -> fromBool (f (toBool v) (toBool v')))
+evalExp (OpUn (UnInt f) e)        = do v <- evalExp e
+                                       return (f v)
+evalExp (OpUn (UnBool f) e)       = do v <- evalExp e
+                                       return (fromBool (f (toBool v)))
 
 
-evalBoolOp e1 e2 f = do v1 <- evalExp e1
-                        v2 <- evalExp e2
-                        return (boolToInt (f v1 v2))
 
-evalIntOp e1 e2 f = do v1 <- evalExp e1
-                       v2 <- evalExp e2
-                       return (f v1 v2)
+evalOp e1 e2 f = do v1 <- evalExp e1
+                    v2 <- evalExp e2
+                    return (f v1 v2)
   
-boolToInt False = 0
-boolToInt True = 1
+fromBool False = 0
+fromBool True = 1
 
-andOp 0 x = 0
-andOp x 0 = 0
-andOp x y = 1
-
-orOp 0 0 = 0
-orOp x y = 1
-
----------------------------------------
--- Term evaluator
----------------------------------------
-      
-
--- Evaluates multiple steps of a command, until it reaches Skip
-evalStm :: Stm a b c d -> Result ()
-evalStm Skip          = return ()
-evalStm (Ass x e)     = do v <- evalExp e
-                           update (toInt x) v
-
-evalStm (Seq c0  c1)  =  evalStm c0 >> (evalStm c1) 
-evalStm (If b c0 c1)  = do vb <- evalExp b
-                           if vb == 1 then evalStm c0 else evalStm c1 
-evalStm w@(While b c) = do vb <- evalExp b
-                           if vb == 1 then evalStm (Seq c w) else return () 
-
-
-
-
-
+toBool 1 = True
+toBool 0 = False
