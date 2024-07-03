1a8168fe71ef3c1407d768f63ef4d30978b6a311
ceciliamanzino <70380080+ceciliamanzino@users.noreply.github.com>
2024-07-03 15:43:47 -0300

    Update avg-attack.hs

--- avg-attack.hs
+++ avg-attack.hs
@@ -1,7 +1,8 @@
-import DR.AbstractSyntax
-import DR.Constructors
-import DR.Environment
-import DR.Interpreter
+import AbstractSyntax
+import Constructors
+import Environment
+import Interpreter
+import TwoLevels
 
 import qualified Data.Map.Strict as M
 
@@ -26,19 +27,24 @@ avg := declassify((h1 + · · · + hn)/n, low)
 -- Security environment for this example
 ------------------------------------------
 
-env = (zero, L) :-: (one, H) :-: (two, H) :-: (three, H) :-: (four, H) :-: Nil
+-- env = (zero, L) :-: (one, H) :-: (two, H) :-: (three, H) :-: (four, H) :-: Nil
 
 -- Variables
-avg = var env zero
-h1 = var env one  
-h2 = var env two 
-h3 = var env three 
+(avg', env1) = newVar L initEnv 
+(h1' , env2) = newVar H env1  
+(h2' , env3) = newVar H env2
+(h3 , env) = newVar H env3
+
+-- actualize evironmet in variables
+avg = updateEnv env avg' 
+h1 = updateEnv env h1'
+h2 = updateEnv env h2'
 
 -----------------------
 -- Secure program
 -----------------------
 
-avgSalaries = zero =: declassify ((h1 +. h2 +. h3) //. int 3) L
+avgSalaries =  avg =: declassify ((h1 +. h2 +. h3) //. int 3) L
 
 -- Evaluation
 
@@ -53,13 +59,8 @@ res = eval avgSalaries memory --  result:  fromList [(0,5),(1,10),(2,3),(3,2)]
 
 -- This program is rejected by the Haskell type checker
 
-{-
-unsecureProgram = one =: int 100 >>>   -- Swapping the values h1, h2.
-                  two =: h1 >>>
-                  three =: h1 >>>
+
+unsecureProgram = h2 =: h1 >>>
+                  h3 =: h1 >>>
                   avgSalaries 
--}
-
-
-
 
